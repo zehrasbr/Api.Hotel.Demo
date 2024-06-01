c000958923093159e506e3a70a9d78c07793fac7
zehrasbr <zehraisbr@gmail.com>
2024-06-01 23:18:44 +0300

    api

---
+++ ApiConsume/HotelProject.WebApi/Controller/RoomController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult RoomList()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult AddRoom()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteRoom()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateRoom()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetRoom()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ApiConsume/HotelProject.WebApi/Controller/StaffController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult StaffList()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult AddStaff()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStaff()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateStaff()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetStaff()
+        {
+            return Ok();
+        }
+    }
+}
