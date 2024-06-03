e3838ecf9f8d6d0769196fb3209b1ee7d17aacf8
zehrasbr <zehraisbr@gmail.com>
2024-06-03 20:07:23 +0300

    layout

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+    }
+}
