479436a8915b1fb172b276cc37d436b763748dfc
zehrasbr <zehraisbr@gmail.com>
2024-06-03 20:36:08 +0300

    partial

--- Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminLayoutController.cs
@@ -8,5 +8,33 @@ namespace HotelProject.WebUI.Controllers
         {
             return View();
         }
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PreloaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavheaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
     }
 }
