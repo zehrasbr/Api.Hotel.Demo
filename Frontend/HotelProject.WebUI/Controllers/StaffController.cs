755d1cf5dff1554c8285a11fb59d481947a5ce5d
zehrasbr <zehraisbr@gmail.com>
2024-06-05 19:58:43 +0300

    .

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -1,6 +1,5 @@
 ﻿using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.ProjectModel;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -11,17 +10,15 @@ namespace HotelProject.WebUI.Controllers
     public class StaffController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
-
         public StaffController(IHttpClientFactory httpClientFactory)
         {
-            _httpClientFactory=httpClientFactory;
+            _httpClientFactory = httpClientFactory;
         }
-
         public async Task<IActionResult> Index()
         {
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("http://localhost:46246/api/Staff");
-            if(responseMessage.IsSuccessStatusCode)
+            if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
--- Frontend/HotelProject.WebUI/Startup.cs
+++ Frontend/HotelProject.WebUI/Startup.cs
@@ -1,3 +1,4 @@
+using HotelProject.WebUI.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -23,6 +24,7 @@ namespace HotelProject.WebUI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
