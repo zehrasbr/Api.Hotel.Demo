9d503b4d54cfdf70683aa985a95e5904379c31ba
zehrasbr <zehraisbr@gmail.com>
2024-06-09 22:02:22 +0300

    mapper

---
+++ ApiConsume/HotelProject.WebApi/Controller/Room2Controller.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Room2Controller : ControllerBase
+    {
+        private readonly IRoomService _roomService; 
+        private readonly IMapper _mapper;
+        public Room2Controller(IRoomService roomService, IMapper mapper)
+        {
+            _roomService=roomService;
+            _mapper=mapper;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _roomService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddRoom(RoomAddDto roomAddDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var values = _mapper.Map<Room>(roomAddDto);
+            var result = _roomService.TInsert(values);
+            if(result == null)
+            {
+                return BadRequest();
+            }
+            return Ok(_mapper.Map<RoomAddDto>(result));
+        }
+    }
+}
