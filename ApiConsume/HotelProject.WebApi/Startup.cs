6157721b894b13d838c1da7804b8b15a1bfb988f
zehrasbr <zehraisbr@gmail.com>
2024-06-09 21:53:20 +0300

    mapping automapper

--- ApiConsume/HotelProject.WebApi/Startup.cs
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -46,6 +46,8 @@ namespace HotelProject.WebApi
             services.AddScoped<ITestimonialDal, EfTestimonialDal>();
             services.AddScoped<ITestimonialService, TestimonialManager>();
 
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddCors(opt =>
             {
                 opt.AddPolicy("OtelApiCors", opts =>
