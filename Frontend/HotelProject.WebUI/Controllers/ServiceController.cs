d70fd2fd145c2e0f6e0a3d33c15d14fbcf98b694
zehrasbr <zehraisbr@gmail.com>
2024-06-11 19:31:07 +0300

    service

--- Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -1,11 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class ServiceController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory=httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:46246/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddService()
         {
             return View();
         }
