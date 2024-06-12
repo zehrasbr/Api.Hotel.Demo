9315727a138df1e42c78fff8e75d82bbe7a43fd4
zehrasbr <zehraisbr@gmail.com>
2024-06-12 20:24:08 +0300

    register

---
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager=userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AppUser)
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage ="Ad alanı gereklidir")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Soyad alanı gereklidir")]
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Kullanıcı adı alanı gereklidir")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Mail alanı gereklidir")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Şifre alanı gereklidir")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Şifre tekrar alanı gereklidir")]
+        [Compare("Password",ErrorMessage ="Şifreler Uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+    }
+}
