d03fdc97b3c32fe95d9d3562cc00272a252b25c1
zehrasbr <zehraisbr@gmail.com>
2024-06-12 20:40:28 +0300

    .

--- Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.RegisterDto;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -17,8 +19,24 @@ namespace HotelProject.WebUI.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Index(AppUser)
+        public async Task<IActionResult> Index(CreateNewUserDto createNewUserDto)
         {
+            if(ModelState.IsValid)
+            {
+                return View();
+            }
+            var appUser = new AppUser()
+            {
+                Name = createNewUserDto.Name,
+                Email = createNewUserDto.Mail,
+                Surname = createNewUserDto.Surname,
+                UserName = createNewUserDto.Username
+            };
+            var result =await _userManager.CreateAsync(appUser, createNewUserDto.Password);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index","Login");
+            }
             return View();
         }
     }
--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
 
 namespace HotelProject.WebUI.Mapping
@@ -11,6 +12,8 @@ namespace HotelProject.WebUI.Mapping
                 CreateMap<ResultServiceDto, Service>().ReverseMap();
                 CreateMap<UpdateServiceDto, Service>().ReverseMap();
                 CreateMap<CreateServiceDto, Service>().ReverseMap();
+
+                CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
         }
     }
 }
