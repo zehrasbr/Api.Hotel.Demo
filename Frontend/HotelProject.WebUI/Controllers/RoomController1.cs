38f9be3f5afe95e0c0627db60049e4e5a7873c46
zehrasbr <zehraisbr@gmail.com>
2024-06-19 21:45:54 +0300

    .

---
+++ Frontend/HotelProject.WebUI/Controllers/RoomController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RoomController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
