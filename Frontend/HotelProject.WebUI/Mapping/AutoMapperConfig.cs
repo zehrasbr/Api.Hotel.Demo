9dd6268573a691dc5b8c3a4c64ab18cea6d9a4cd
zehrasbr <zehraisbr@gmail.com>
2024-06-22 11:04:54 +0300

    booking

--- Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.AboutDto;
+using HotelProject.WebUI.Dtos.BookingDto;
 using HotelProject.WebUI.Dtos.LoginDto;
 using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.RoomDto;
@@ -35,6 +36,8 @@ namespace HotelProject.WebUI.Mapping
                 CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
 
                 CreateMap<CreateSubscribeDto, Subscribe>().ReverseMap();
+
+                CreateMap<CreateBookingDto, Book>().ReverseMap();
         }
     }
 }
