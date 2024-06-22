39133f415bd08e674f1b111769be3bc92f1d217c
zehrasbr <zehraisbr@gmail.com>
2024-06-22 11:26:07 +0300

    booking api cont

---
+++ ApiConsume/HotelProject.WebApi/Controller/BookingController.cs
@@ -0,0 +1,49 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService=bookingService;
+        }
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingService.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddBooking(Booking Booking)
+        {
+            _bookingService.TInsert(Booking);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteBooking(int id)
+        {
+            var values = _bookingService.TGetByID(id);
+            _bookingService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateBooking(Booking Booking)
+        {
+            _bookingService.TUpdate(Booking);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id)
+        {
+            var values = _bookingService.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
--- ApiConsume/HotelProject.WebApi/Startup.cs
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -49,6 +49,9 @@ namespace HotelProject.WebApi
             services.AddScoped<IAboutDal, EfAboutDal>();
             services.AddScoped<IAboutService, AboutManager>();
 
+            services.AddScoped<IBookingDal, EfBookingDal>();
+            services.AddScoped<IBookingService, BookingManager>();
+
             services.AddAutoMapper(typeof(Startup));
 
             services.AddCors(opt =>
